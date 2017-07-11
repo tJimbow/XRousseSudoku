c7e3c14352b8ab024553c62f655264d7da0f6c81
tJimbow <triomphe.jeanmarie@gmail.com>
2017-07-11 18:20:26 +0200

    Add Main Menu

--- XRousseSudoku/XRousseSudoku/MainPage.xaml.cs
+++ XRousseSudoku/XRousseSudoku/MainPage.xaml.cs
@@ -9,13 +9,14 @@ namespace XRousseSudoku
 {
     public partial class MainPage : ContentPage
     {
+        Label label;
+
         public MainPage()
         {
-
             InitializeComponent();
             BackgroundColor = Color.FromHex("#e5c15e");
 
-            Content = new Label
+            Label header = new Label
             {
                 Text = "XRousse Sudoku",
                 FontSize = 40,
@@ -25,6 +26,58 @@ namespace XRousseSudoku
                 VerticalOptions = LayoutOptions.StartAndExpand,
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
             };
-        }  
-	}
+
+            Slider slider = new Slider
+            {
+                Minimum = 0,
+                Maximum = 5,
+                WidthRequest = 5,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+            slider.ValueChanged += OnSliderValueChanged;
+
+            label = new Label
+            {
+                Text = "Slider value is 0",
+                FontSize = 20,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Button newGameBtn = new Button
+            {
+                Text = "Nouvelle partie",
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            // Accomodate iPhone status bar.
+            Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            // Build the page.
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    header,
+                    slider,
+                    label,
+                    newGameBtn
+                }
+            };
+
+            RelativeLayout relative = new RelativeLayout();
+
+            ScrollView scroll = new ScrollView {
+                Content = {}
+            };
+
+
+        }
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            label.Text = String.Format("Slider value is {0:F1}", e.NewValue);
+        }
+    }
 }
