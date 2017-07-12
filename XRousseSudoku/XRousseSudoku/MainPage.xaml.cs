46c9850ef219ff3f65778c35d54ead0dca95676e
Victor Bernot <victor.bernot@gmail.com>
2017-07-12 15:54:38 +0200

    finish cleaning up Jim code

--- XRousseSudoku/XRousseSudoku/MainPage.xaml.cs
+++ XRousseSudoku/XRousseSudoku/MainPage.xaml.cs
@@ -7,100 +7,109 @@ using Xamarin.Forms;
 
 namespace XRousseSudoku
 {
-    // Add class slider with Label Update Method with difficulty list
+    // SliderStep: slider with custom step integer value + custom Label + associating a name to each value
     public class SliderStep : Slider
     {
-        Label label;
-        private int StepValue;
-        private Label sliderMessage;
+        private Label _label;
+        private int _stepValue;
+        private List<string> _valueNames;
+        private String _valueNamePrefix;
 
-        // Construct Slider
-        public SliderStep(float inMin, float inMax, Label inMessage, int step=1)
+        public Label Label { get => _label; }
+
+        public SliderStep(
+            float inMin, 
+            float inMax, 
+            Label label,
+            List<string> valueNames,
+            string valueNamePrefix,
+            int step=1)
         {
-            // Margin for the slider thickness(left,top,right,bottom)
-            Margin = new Thickness(100, 0, 100, 0);
-            // Int : step value for the slide
-            StepValue = step;
-            // Label : message label object from constructor
-            sliderMessage = inMessage;
-            // Add options for the slider
-            // float Min, Max and default value for the slider
+            // init members
             Maximum = inMax;
             Minimum = inMin;
-            setDefaultValue();
-            // Slider is Center and Expand with parent
+            _label = label;
+            _stepValue = step;
+            _valueNames = valueNames;
+            _valueNamePrefix = valueNamePrefix;
+
+            // Margin for the slider thickness(left,top,right,bottom)
+            Margin = new Thickness(100, 0, 100, 0);
+
+            // Slider is Centered and Expands with parent
             VerticalOptions = LayoutOptions.CenterAndExpand;
+
+            // set slider min / max & value
+            SetDefaultValue();
+            
+            // 
             UpdateText((int)Value);
+
+            //
+            ValueChanged += OnSliderValueChanged;
         }
 
-        public void setDefaultValue()
+        public void SetDefaultValue()
         {
             // Calculating the average between min and max slider value
-            double moyenne = (Minimum + Maximum) / 2;
-            Value = (int) moyenne;
+            double average = (Minimum + Maximum) / 2.0;
+            Value = (int)average;
         }
-
-        // Creating List for the difficulties
-        List<string> difficultyList = new List<string>()
-        {
-            "Facile",
-            "Normal",
-            "Difficile"
-        };
-
 
         // Method to change the value of message slider with the difficulty list
         public void UpdateText(int value)
         {
-            sliderMessage.Text = String.Format("Niveau de difficulté : {0:F1}", difficultyList[value - 1]);
+            Label.Text = _valueNamePrefix + " " + _valueNames[value - 1];
         }
 
        // Method to change value from slide only to int value
         public void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
         {
             // Round value between the new value on Step Value
-            var newStep = Math.Round(e.NewValue / StepValue);
+            var newStep = Math.Round(e.NewValue / _stepValue);
+
             // Affect it to the slide value
-            this.Value = newStep * StepValue;
+            this.Value = newStep * _stepValue;
+
             // Call method to display the new difficulty level
             UpdateText((int)Value);
         }
-
     }
 
     public partial class MainPage : ContentPage
     {
+        private SliderStep _difficultySlider;
 
         public MainPage()
         {
+            // must stay first
             InitializeComponent();
 
+            // set background image
             BackgroundImage = "retina_wood_1024.png";
         
             // Build the page.
             bool testGrid = false;
             if (testGrid)
+            {
                 // Display Grid
                 Content = Grid4x4_Test();
+            }
             else
             {
                 // Or display Menu
-                Content = mainMenu();
+                Content = MainMenu();
             }
-        }
-
-        SliderStep difficultySlider;
+        }        
 
         protected override void OnAppearing()
         {
-            difficultySlider.setDefaultValue();
+            // must set slider value here because of Xamarin bug when slider.Minimum != 0
+            _difficultySlider.SetDefaultValue();
         }
 
-        public View mainMenu()
+        public View MainMenu()
         {
-            // Value min and max to initiate Slider
-            float minSlider = 1.0f;
-            float maxSlider = 3.0f;
             // Create title view
             Label mainTitleView = new Label
             {
@@ -116,7 +125,7 @@ namespace XRousseSudoku
             };
 
             // Create the difficulty message view
-            Label difficultyMessage = new Label
+            Label difficultyLabel = new Label
             {
                 // Add options for the difficulty message
                 Margin = new Thickness(10, 0, 10, 10),
@@ -127,10 +136,17 @@ namespace XRousseSudoku
             };
 
             // Create slider view
-            difficultySlider = new SliderStep(minSlider, maxSlider, difficultyMessage);
-            // Change value from the difficultySlider method OnSliderValueChanged when slider is moved
-            difficultySlider.ValueChanged += difficultySlider.OnSliderValueChanged;
-
+            float minSlider = 1.0f;
+            float maxSlider = 3.0f;
+            List<string> difficultyNames = new List<string>()
+            {
+                "Facile",
+                "Normal",
+                "Difficile"
+            };
+            string prefix = "La difficulté est :";
+            _difficultySlider = new SliderStep(minSlider, maxSlider, difficultyLabel, difficultyNames, prefix);
+            
             // Create new button view to create a new game
             Button newGameBtn = new Button
             {
@@ -154,9 +170,9 @@ namespace XRousseSudoku
                         // Add title
                         mainTitleView,
                         // Add slider
-                        difficultySlider,
+                        _difficultySlider,
                         // Add message for the slider
-                        difficultyMessage,
+                        _difficultySlider.Label,
                         // Add button to create new game
                         newGameBtn
                     }
