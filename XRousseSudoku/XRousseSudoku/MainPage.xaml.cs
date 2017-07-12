332e89293daf6e6b5903c8b3b2ff518a63f3b4d4
Victor Bernot <victor.bernot@gmail.com>
2017-07-12 20:44:13 +0200

    Change to GamePage when click on play button

--- XRousseSudoku/XRousseSudoku/MainPage.xaml.cs
+++ XRousseSudoku/XRousseSudoku/MainPage.xaml.cs
@@ -20,17 +20,7 @@ namespace XRousseSudoku
             BackgroundImage = "retina_wood_1024.png";
         
             // Build the page.
-            bool testGrid = false;
-            if (testGrid)
-            {
-                // Display Grid
-                Content = Grid4x4_Test();
-            }
-            else
-            {
-                // Or display Menu
-                Content = MainMenu();
-            }
+            Content = MainMenu();
         }        
 
         protected override void OnAppearing()
@@ -81,6 +71,7 @@ namespace XRousseSudoku
                 VerticalOptions   = LayoutOptions.CenterAndExpand
             };
 
+            newGameBtn.Clicked += OnNewGame;
 
             // Create new Layout with all the Views
             StackLayout contentMenu = new StackLayout
@@ -103,46 +94,18 @@ namespace XRousseSudoku
             return contentMenu;
         }
 
-        public View Grid4x4_Test()
+        async void OnNewGame(object sender, EventArgs ea)
         {
-            // create a grid
-            Grid grid = new Grid();
+            var modalPage = new Page1();
+            
+            await Navigation.PushModalAsync(modalPage);
+            System.Diagnostics.Debug.WriteLine("The modal page is now on screen");
+            
+            //var poppedPage = await Navigation.PopModalAsync();
 
-            // add 2 lines and 2 columns 
-            // (do not interleave RowDefinitions.Add & ColumnDefinitions.Add calls, this fails !)
-            for (int i = 0; i < 2; i++)
-                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-            for (int j = 0; j < 2; j++)
-                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-
-            // set content of each cell
-            for (int i = 0; i < 2; i++)
-            {
-                for (int j = 0; j < 2; j++)
-                {
-                    // each cell contains a layout frame (in expand all mode)
-                    Frame frame = new Frame();
-                    frame.VerticalOptions   = LayoutOptions.FillAndExpand;
-                    frame.HorizontalOptions = LayoutOptions.FillAndExpand;
-                    //frame.OutlineColor = Color.Red; // only has effect on uwp
-                    frame.BackgroundColor = (((i + j)% 2) == 0) ? Color.White : Color.Red;
-
-                    // the frame contains a centered label
-                    string label_text = Char.ConvertFromUtf32(65 + i + j * 2);
-                    frame.Content = new Label
-                    {
-                        Text = label_text,
-                        VerticalOptions   = LayoutOptions.Center,
-                        HorizontalOptions = LayoutOptions.Center,
-                        FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
-                    };
-
-                    // add frame to the grid
-                    grid.Children.Add(frame, i, j);
-                }
-            }
-            return grid;
-        }
+            //Debug.WriteLine("The modal page is dismissed");
+            //Debug.WriteLine(Object.ReferenceEquals (modalPage, poppedPage)); //prints "true"
+        }        
 
         //static void myPage_SizeChanged(object sender, EventArgs e)
         //{
